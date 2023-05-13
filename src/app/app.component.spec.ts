0de6bad70b8423e765339a80b7a18fc321ba7dac
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-05-13 04:12:07 +0100

    set up route to login component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'opensky-api-angular-project'`, () => {
+  it(`should have as title 'Hamoye flights tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('opensky-api-angular-project');
+    expect(app.title).toEqual('Hamoye flights tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('opensky-api-angular-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Hamoye flights tracker app is running!'
+    );
   });
 });
